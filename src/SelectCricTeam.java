95fac47065139c5e83bc7a1cdf5d1247fd276189
asanka sanjaya <angal.sh@gmail.com>
2015-10-14 21:35:54 +0530

    adding InCountryBatStatsScoreMapper

--- src/SelectCricTeam.java
+++ src/SelectCricTeam.java
@@ -232,9 +232,9 @@ public class SelectCricTeam {
 					}
 
 					int sixes =0;
-					if (!tokens[13].equals("-")) {
+					/*if (!tokens[13].equals("-")) {
 						sixes= Integer.parseInt(tokens[13]);
-					}
+					}*/
 
 					String playerName = tokens[0];
 					float weightedScore = average * 11 + hs *4  + sr * 5 + innings
