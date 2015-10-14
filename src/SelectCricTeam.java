8f447104f2db15ff78d66eb8830d0a962c091687
asanka sanjaya <angal.sh@gmail.com>
2015-10-14 21:32:06 +0530

    adding InCountryBatStatsScoreMapper

--- src/SelectCricTeam.java
+++ src/SelectCricTeam.java
@@ -189,6 +189,7 @@ public class SelectCricTeam {
 		public void map(Object key, Text value, Context context)
 				throws IOException, InterruptedException {
 			String line = value.toString();
+			System.out.println(line);
 			if (line.length() > 0) {
 
 				String[] tokens = line.split(",");
